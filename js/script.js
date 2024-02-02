bc9403a745f69c7ad4bab5e69030dd6548c52124
AlbertoCavans <alberto96cavani@gmail.com>
2024-02-02 18:07:35 +0100

    Feat script.js - methods

--- js/script.js
+++ js/script.js
@@ -4,35 +4,60 @@ const app = createApp({
     return {
       slides: [
         {
-          image: "img/01.webp",
+          image: "./img/01.webp",
           title: "Marvel's Spiderman Miles Morale",
           text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
         },
         {
-          image: "img/02.webp",
+          image: "./img/02.webp",
           title: "Ratchet & Clank: Rift Apart",
           text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
         },
         {
-          image: "img/03.webp",
+          image: "./img/03.webp",
           title: "Fortnite",
           text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
         },
         {
-          image: "img/04.webp",
+          image: "./img/04.webp",
           title: "Stray",
           text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
         },
         {
-          image: "img/05.webp",
+          image: "./img/05.webp",
           title: "Marvel's Avengers",
           text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
         },
       ],
+
       observedImg: 0,
-      trueFalse: true,
     };
   },
-  mounted() {},
+
+  methods: {
+    goToSlide(index) {
+      this.observedImg = index;
+    },
+
+    prevSlide() {
+      if (this.observedImg <= 0) {
+        this.goToSlide(this.slides.lenght - 1);
+      } else {
+        this.goToSlide(this.observedImg - 1);
+      }
+    },
+
+    nextSlide() {
+      if (this.observedImg >= this.slides.lenght - 1) {
+        this.goToSlide(0);
+      } else {
+        this.goToSlide(this.observedImg + 1);
+      }
+    },
+  },
+
+  mounted() {
+    console.log("app montata");
+  },
 });
 app.mount("#app");
